e380824a68e170f2e286950eb32330b550860e79
Nadim Rai <nadim_rai@yahoo.com>
2023-10-17 00:22:38 +0800

    configured html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
-const path = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
