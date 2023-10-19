fbbb8a6a7229a38736250bd5ba3ffd35c568dbeb
Nadim Rai <nadim_rai@yahoo.com>
2023-10-20 01:59:56 +0800

    refactored code by creating postData js file

--- src/client/js/tripCity.js
+++ src/client/js/tripCity.js
@@ -1,24 +1,9 @@
+import { postData } from "./postData";
+
 const tripCity = async (city) =>{
     const data =  await postData("http://localhost:3000/location", { city: city })
     console.log("frontend", data);
     return data
-}
-
-const postData = async ( url = "", data = {})=>{
-    const response = await fetch(url, {
-        method: 'POST', 
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-      body: JSON.stringify(data), 
-    });
-    try {
-        const newData = await response.json();
-        return newData;
-    }catch(error) {
-      console.log("error", error);
-    }
 }
 
 export { tripCity }
--- src/client/js/tripPicture.js
+++ src/client/js/tripPicture.js
@@ -1,3 +1,5 @@
+import { postData } from "./postData";
+
 const tripPicture = async (city_name) => {
     const { data } = await postData("http://localhost:3000/picture", {
       city_name,
@@ -5,22 +7,5 @@ const tripPicture = async (city_name) => {
     const { image } = await data;
     return image;
   };
-
-  const postData = async ( url = "", data = {})=>{
-    const response = await fetch(url, {
-        method: 'POST', 
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-      body: JSON.stringify(data), 
-    });
-    try {
-        const newData = await response.json();
-        return newData;
-    }catch(error) {
-      console.log("error", error);
-    }
-}
 
 export { tripPicture }
