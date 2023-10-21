06deb5851cf370f7fdc31ba38cff80b079956159
Nadim Rai <nadim_rai@yahoo.com>
2023-10-22 01:27:43 +0800

    created button click effect

--- src/client/js/tripDays.js
+++ src/client/js/tripDays.js
@@ -3,12 +3,13 @@ const tripDays = (date) => {
     const startDate = new Date();
     const endDate = new Date(date);
   
-    const timeDiff = endDate.getTime() - startDate.getTime();
+    const time_difference = endDate.getTime() - startDate.getTime();
   
-    const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    console.log("days",daysDiff);
-
-    return daysDiff;
+    const day_difference = Math.ceil(time_difference / (1000 * 3600 * 24));
+  
+    return day_difference;
   };
 
 export{ tripDays }
+
+//https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
